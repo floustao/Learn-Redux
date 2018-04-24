2afd5915d8363631f0b207324367462d2e225ab5
Florestan Lefevre <florestanlefevre@gmail.com>
2018-04-24 16:26:06 -0700

    Display redux state in Photo component and routing to Photo page with Link

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -8,7 +8,7 @@ class Main extends React.Component {
     return (
       <div>
         <h1>
-          <Link to="/">Reduxstagram</Link>
+          <Link to="/">Hatstagram</Link>
         </h1>
         {React.cloneElement(this.props.children, this.props)}
       </div>
