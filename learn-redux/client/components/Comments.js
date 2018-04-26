630fa435a33454916acf3b9a2f30216e979391da
Florestan Lefevre <florestanlefevre@gmail.com>
2018-04-26 11:23:27 -0700

    Display photo page and comments

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Comments extends React.Component {
+  render() {
+    return (
+      <p>Comments....</p>
+    );
+  }
+}
+
+export default Comments;
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+
+
 
 class Single extends React.Component {
   render() {
+
+    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    console.log(post);
+
     return (
       <div className="single-photo">
-        Single
+        <Photo post={post} i={i} {...this.props}/>
+        <Comments />
       </div>
     );
   }
